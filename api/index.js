d0c082167acd9eafe7e437099e267c754b9ebf7d
senseihimanshu <hsharma151197@gmail.com>
2020-02-15 12:15:01 +0530

    Added jwt authentication with login signup

---
+++ api/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const config = require('config');
+const authenticate = require('./middlewares/authentication');
+const signupRoutes = require('./routes/signup');
+const loginRoutes = require('./routes/login');
+
+if(!config.get('jwtPrivateKey')){
+    console.log(config.get('jwtPrivateKey'));
+    console.error('FATAL ERROR: secretKey not set');
+    process.exit(1);
+}
+
+mongoose.connect('mongodb://localhost/twitter').then(() => console.log('MongoDb connected')).catch(err => console.error('Error occured while connecting to db', err));
+
+app.use(express.json());
+app.use(authenticate);
+
+app.use('/api/signup', signupRoutes);
+app.use('/api/login', loginRoutes);
+
+const port = process.env.PORT || 3000;
+app.listen(port, ()=> console.log(`Listening at port ${port}`));
