abe755b7d9111b407bee8442ef8001acd8d45478
horaanchal <horaanchal@gmail.com>
2020-03-17 16:17:33 +0530

    feed component

--- tweet-tweet/src/app/app.module.ts
+++ tweet-tweet/src/app/app.module.ts
@@ -57,6 +57,7 @@ import { ProfileComponent } from './profile/profile.component';
 import { CommonModule } from '@angular/common';
 import { ReplyComponent } from './home/components/reply/reply.component';
 import { SearchComponent } from './home/components/search/search.component';
+import { FeedComponent } from './feed/feed.component';
 
 //
 
@@ -78,6 +79,7 @@ import { SearchComponent } from './home/components/search/search.component';
     ProfileComponent,
     ReplyComponent,
     SearchComponent,
+    FeedComponent,
   ],
   imports: [
     //Ng Modules
