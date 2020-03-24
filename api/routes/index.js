e4f8528816b70c2dd762ded8c4467de1b4cee03b
horaanchal <horaanchal@gmail.com>
2020-03-25 00:41:18 +0530

    bio added in schema and update route added

--- api/routes/index.js
+++ api/routes/index.js
@@ -9,4 +9,5 @@ module.exports = (app) => {
     app.post('/signup', controller.user.signup);
     app.post('/login', controller.user.login);
     app.get('/profile', authenticator, controller.user.getProfile);
+    app.put('/profile/:id', authenticator, controller.user.updateProfile);
 }
